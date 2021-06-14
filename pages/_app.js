11f2c41b34ddf596795dc6b6eddc11cc67b4a06a
Nick Oelsner <noelsner@gmail.com>
2021-06-14 17:48:27 -0500

    add font, add color variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -13,25 +13,16 @@ export default function MyApp({ Component, pageProps }) {
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <title>brewratio.coffee</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
+        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
+        <link rel="preload" href="/assetsfonts/Inter-VariableFont_slnt,wght.ttf" as="font" crossOrigin="" />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
