1572798abde24dd3615107a4865883426368cf77
Nick Oelsner <noelsner@gmail.com>
2021-06-14 17:48:49 -0500

    add logo svg

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>welcome to brewratio.coffee</h1>
+        <img src="/assets/images/Logo.svg" alt="coffee cup logo" width="164" height="auto" />
       </main>
     </div>
   );
