28f8240a70a56b3dd7056de16f2d90ec95664bff
Nick Oelsner <noelsner@gmail.com>
2021-07-01 23:08:39 -0700

    add plausible script

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" title="brewgood.coffee">
-        <Head>
-          <title>brewgood.coffee</title>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
+import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>recipes for brewing the perfect cup of coffee</h1>
-        <img src="/assets/images/Logo.svg" alt="coffee cup logo" className={styles.logo} />
-        <Link href="/brewMethods">
-          <a className={styles.button}>
-            <span>start brewing</span>
-            <img src="/assets/images/arrow.svg" alt="chevron right" />
-          </a>
-        </Link>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>brewgood.coffee</title>
+        <script defer data-domain="brewgood.coffee" src="https://plausible.io/js/plausible.js"></script>
+      </Head>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>recipes for brewing the perfect cup of coffee</h1>
+          <img src="/assets/images/Logo.svg" alt="coffee cup logo" className={styles.logo} />
+          <Link href="/brewMethods">
+            <a className={styles.button}>
+              <span>start brewing</span>
+              <img src="/assets/images/arrow.svg" alt="chevron right" />
+            </a>
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }
