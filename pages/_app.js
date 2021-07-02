6a291f1de0beb0f0d5ee23c89733edeb6afb1b9e
Nick Oelsner <noelsner@gmail.com>
2021-07-01 17:19:42 -0700

    add footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <Component {...pageProps} />
+      </div>
+      <footer>
+        made with ❤️ and ☕️ by <a href="https://twitter.com/NickOelsner">nick oelsner</a>
+      </footer>
+    </div>
+  );
 }
